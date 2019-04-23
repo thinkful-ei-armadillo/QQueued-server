6d34adce374c2a7264abcc2e49a68667cd2cd5dd
Robin Khiv <robinkhiv@gmail.com>
2019-04-23 14:37:44 -0700

    fixed sql quote error

--- migrations/001.do.create_user.sql
+++ migrations/001.do.create_user.sql
@@ -2,6 +2,6 @@ CREATE TABLE "user" (
   "id" SERIAL PRIMARY KEY,
   "username" TEXT NOT NULL UNIQUE,
   "title" TEXT NOT NULL,
-  "name" TEXT NOT NULL
-  "password" TEXT NOT NULL,
+  "name" TEXT NOT NULL,
+  "password" TEXT NOT NULL
 );
