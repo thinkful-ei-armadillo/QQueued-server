f95392594191e6bef311ab26cab14a68bc13bb6a
Robin Khiv <robinkhiv@gmail.com>
2019-04-25 12:40:04 -0700

    post endpoint to enqueue server

--- src/routes/queue/queueService.js
+++ src/routes/queue/queueService.js
@@ -17,8 +17,32 @@ const QueueService ={
       .where({completed: false})
       .rightJoin('user','queue.user_name', 'user.user_name')
       .leftJoin('user AS mentor', 'queue.mentor_user_name', 'mentor.user_name');
+  },
+  enqueue(db, data){
+    return db
+      .insert(data)
+      .into('queue')
+      .returning('*')
+      .then(([queue]) => queue)
+      .then(queue =>
+        QueueService.getById(db, queue.id)
+      );
+  },
+  getById(db, id) {
+    return QueueService.getAll(db)
+      .where('queue.id', id)
+      .first();
+  },
+  updateQueue(db, idToUpdate, newTailId){
+    return db
+      .into('queue')
+      .where({ id: idToUpdate })
+      .update({next: newTailId});
+  },
+  updateTailPointer(db, newTailId){
+    return db.into('pointers').update({tail: newTailId});
   }
 };
 
-//slack_user-id
+
 module.exports = QueueService;
