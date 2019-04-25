c0bc9c97e75f9f0ff209ef7653d5004c7ab32e4c
matth3wn <matth3wn@gmail.com>
2019-04-25 09:06:41 -0700

    took off not null on user_name

--- migrations/002.do.create_queue.sql
+++ migrations/002.do.create_queue.sql
@@ -2,13 +2,13 @@ CREATE TABLE "queue" (
   "id" SERIAL PRIMARY KEY,
   "description" TEXT NOT NULL,
   "user_name" TEXT REFERENCES "user"(user_name)
-    ON DELETE CASCADE NOT NULL,
+    ON DELETE CASCADE,
   "mentor_user_name" TEXT REFERENCES "user"(user_name)
     ON DELETE CASCADE DEFAULT NULL, 
   "dequeue" BOOLEAN NOT NULL DEFAULT FALSE,
   "completed" BOOLEAN NOT NULL DEFAULT FALSE,
   "slack_user_id" TEXT,
+  "slack_handle"  TEXT,
   "next" INTEGER REFERENCES "queue"(id)
     ON DELETE SET NULL
-  
 );
