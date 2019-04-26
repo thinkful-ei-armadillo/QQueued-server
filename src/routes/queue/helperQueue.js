1db7905c00dafd684cbc0c17f3637c2ac3cc577a
matth3wn <matth3wn@gmail.com>
2019-04-26 11:52:10 -0700

    fixed helper funtion

--- src/routes/queue/helperQueue.js
+++ src/routes/queue/helperQueue.js
@@ -1,12 +1,14 @@
 const QueueService = require('./queueService');
 
 const helperQueue = {
-  async getQueueData(req) {
-      const pointer = await QueueService.getPointers(req.app.get('db'));
-      const list = await QueueService.getAll(req.app.get('db'));
-      const queueList = list.filter(listItem => listItem.id >= pointer.head);
-      const currentlyBeingHelped = list.filter(list => list.dequeue === true && list.completed === false);
-
+  async getQueueData(db) {
+    const pointer = await QueueService.getPointers(db);
+    const mentorList = await QueueService.getAll(db);
+    let queueList = [];
+    const currentlyBeingHelped = mentorList.filter(list => list.dequeue === true && list.completed === false);
+    
+    if(pointer.head !== null)
+      queueList = mentorList.filter(listItem => listItem.id >= pointer.head);
       return {queueList, currentlyBeingHelped}
   }
 
