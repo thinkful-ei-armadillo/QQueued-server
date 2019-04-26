7f89c72f93be05befc81af206da9712790d04a20
Robin Khiv <robinkhiv@gmail.com>
2019-04-26 08:20:32 -0700

    updated post and patch queue endpoint for when no one is in line

--- src/routes/queue/queueRoute.js
+++ src/routes/queue/queueRoute.js
@@ -27,21 +27,30 @@ queueRouter
   })
   .post(parser, async (req,res,next)=>{
     try{
-     const pointer = await QueueService.getPointers(req.app.get('db'));
-     const {user_name} = req.user;
-     const {description} = req.body;
-     let newQueueData = {description, user_name };
+      const pointer = await QueueService.getPointers(req.app.get('db'));
+      const {user_name} = req.user;
+      const {description} = req.body;
+      let newQueueData = { description, user_name };
 
-     await QueueService.enqueue(req.app.get('db'), newQueueData)
+      if(!description)
+        return res.status(400).json({
+          error: `Missing description in request body`
+        })
+  
+      await QueueService.enqueue(req.app.get('db'), newQueueData)
        .then(res => newQueueData = res)
 
-     await QueueService.updateQueue(req.app.get('db'), pointer.tail, newQueueData.id)
-     await QueueService.updateTailPointer(req.app.get('db'), newQueueData.id)
-     res.json({
-       studentName: req.user.name
-     })
-     next()
-    }catch (error) {
+      if (pointer.head === null)
+        await QueueService.updateBothPointers(req.app.get('db'), newQueueData.id)
+      else {
+        await QueueService.updateTailPointer(req.app.get('db'), newQueueData.id);
+        await QueueService.updateQueue(req.app.get('db'), pointer.tail, newQueueData.id);
+      } 
+      res.json({
+        studentName: req.user.name
+      })
+
+    } catch (error) {
       next(error);
     }
   })
@@ -56,14 +65,19 @@ queueRouter
       }
 
       const pointer = await QueueService.getPointers(req.app.get('db'));
-      const next = await QueueService.getById(req.app.get('db'), pointer.head);
-      const currentInLine = {mentor_user_name: user_name, dequeue: true, next: null}
+      if(pointer.head === null)
+        return res.json('no students in queue')
+      const current = await QueueService.getById(req.app.get('db'), pointer.head);
+      const currentDequeueUpdate = {mentor_user_name: user_name, dequeue: true, next: null}
       
-      await QueueService.updateHeadPointer(req.app.get('db'), next.next);
-      await QueueService.dequeue(req.app.get('db'), pointer.head, currentInLine);
+      await QueueService.updateHeadPointer(req.app.get('db'), current.next);
+      await QueueService.dequeue(req.app.get('db'), pointer.head, currentDequeueUpdate);
+      if(current.next === null){
+        await QueueService.updateTailPointer(req.app.get('db'), current.next);
+      }
 
-      next()
-
+      res.json('student off queue')
+ 
     } catch (error){
       next(error);
     }
