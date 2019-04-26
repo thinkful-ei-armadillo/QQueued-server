bd8ccf889a1935d65c34d7d695881242ba9d63ef
Hunter Kreshock <hkreshock@gmail.com>
2019-04-26 13:49:15 -0700

    queue route for student list

--- src/routes/queue/queueRoute.js
+++ src/routes/queue/queueRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const QueueService = require('./queueService');
-const {requireAuth} = require('../../middleware/jwt-auth');
+const { requireAuth } = require('../../middleware/jwt-auth');
 const parser = express.json();
 const helperQueue = require('./helperQueue');
 
@@ -8,87 +8,108 @@ const queueRouter = express.Router();
 
 queueRouter
   .route('/')
-  .get( async (req, res, next) => {
-    try{
-      const {queueList, currentlyBeingHelped} =  await helperQueue.getQueueData(req.app.get('db'))
+  .get(async (req, res, next) => {
+    try {
+      const {
+        queueList,
+        currentlyBeingHelped
+      } = await helperQueue.getQueueData(req.app.get('db'));
       res.json({
         queueList,
         currentlyBeingHelped
       });
-
     } catch (error) {
       next(error);
     }
-
   })
-  .post(parser, requireAuth,async (req,res,next)=>{
-    try{
+  .post(parser, requireAuth, async (req, res, next) => {
+    try {
       const pointer = await QueueService.getPointers(req.app.get('db'));
-      const {user_name} = req.user;
-      const {description} = req.body;
+      const { user_name } = req.user;
+      const { description } = req.body;
       let newQueueData = { description, user_name };
 
-      if(!description)
+      if (!description)
         return res.status(400).json({
           error: `Missing description in request body`
-        })
-  
-      await QueueService.enqueue(req.app.get('db'), newQueueData)
-       .then(res => newQueueData = res)
+        });
+
+      await QueueService.enqueue(req.app.get('db'), newQueueData).then(
+        res => (newQueueData = res)
+      );
 
       if (pointer.head === null)
-        await QueueService.updateBothPointers(req.app.get('db'), newQueueData.id)
+        await QueueService.updateBothPointers(
+          req.app.get('db'),
+          newQueueData.id
+        );
       else {
-        await QueueService.updateTailPointer(req.app.get('db'), newQueueData.id);
-        await QueueService.updateQueue(req.app.get('db'), pointer.tail, newQueueData.id);
-      } 
+        await QueueService.updateTailPointer(
+          req.app.get('db'),
+          newQueueData.id
+        );
+        await QueueService.updateQueue(
+          req.app.get('db'),
+          pointer.tail,
+          newQueueData.id
+        );
+      }
       res.json({
-        studentName: req.user.name
-      })
-
+        studentName: req.user.full_name,
+        description: description
+      });
     } catch (error) {
       next(error);
     }
   })
-  .patch(parser, requireAuth ,async (req,res,next)=>{
+  .patch(parser, requireAuth, async (req, res, next) => {
     try {
-      const { title, user_name }  = req.user;
+      const { title, user_name } = req.user;
 
-      if(title !== 'mentor'){
+      if (title !== 'mentor') {
         return res.status(400).json({
           error: `Sorry Only mentors can update queue`
         });
       }
 
       const pointer = await QueueService.getPointers(req.app.get('db'));
-      if(pointer.head === null)
-        return res.status(204)
-      const current = await QueueService.getById(req.app.get('db'), pointer.head);
-      const currentDequeueUpdate = {mentor_user_name: user_name, dequeue: true, next: null}
-      
+      if (pointer.head === null) return res.status(204);
+      const current = await QueueService.getById(
+        req.app.get('db'),
+        pointer.head
+      );
+      const currentDequeueUpdate = {
+        mentor_user_name: user_name,
+        dequeue: true,
+        next: null
+      };
+
       await QueueService.updateHeadPointer(req.app.get('db'), current.next);
-      await QueueService.dequeue(req.app.get('db'), pointer.head, currentDequeueUpdate);
-      if(current.next === null){
+      await QueueService.dequeue(
+        req.app.get('db'),
+        pointer.head,
+        currentDequeueUpdate
+      );
+      if (current.next === null) {
         await QueueService.updateTailPointer(req.app.get('db'), current.next);
       }
 
-      res.status(204)
- 
-    } catch (error){
+      res.status(204);
+    } catch (error) {
       next(error);
     }
-  })
+  });
 
 queueRouter
   .route('/:dequeuedId')
   .all(requireAuth)
-  .patch(async (req,res,next)=>{
-    try{
-      console.log(req.user)
-      console.log('id', req.params.dequeueId)
-    } catch(error) {
-      next(error)
+  .patch(async (req, res, next) => {
+    try {
+      console.log(req.user);
+      console.log('id', req.params.dequeueId);
+    } catch (error) {
+      next(error);
     }
-  })
+  });
 
 module.exports = queueRouter;
