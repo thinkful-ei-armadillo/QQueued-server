fc8cc2d0927c935765ed0b7a894d92f912b372d7
Robin Khiv <robinkhiv@gmail.com>
2019-04-30 09:17:53 -0700

    added fetch to retrieve past data from queue

--- src/routes/data/dataRoute.js
+++ src/routes/data/dataRoute.js
@@ -2,18 +2,18 @@ const express = require('express');
 const dataRouter = express.Router();
 const { requireAuth } = require('../../middleware/jwt-auth');
 const dataService = require('./dataService');
+const QueueSerive = require('../queue/queueService');
 
 dataRouter
   .route('/')
   .get(requireAuth, async (req, res, next) => {
     try {
-      const db = req.app.get('db');
       if (req.user.title !== 'mentor') {
         return res.status(403).send({ error: 'only mentors have access' });
       }
-
-      const data = await dataService.getData(db);
-
+      
+      const data = await dataService.getCompleted(req.app.get('db'))
+      
       res.json(data);
 
     }
--- src/routes/data/dataService.js
+++ src/routes/data/dataService.js
@@ -1,10 +1,20 @@
 const dataService = {
 
-  getData(db) {
-    return db
-      .from('studentData')
-      .select('*');
-  }
+  getCompleted(db) {
+    return db.from('queue')
+      .select(
+        'queue.id',
+        'queue.description',
+        'queue.user_name',
+        'user.full_name as studentName',
+        'mentor.full_name as mentorName'
+      )
+      .where({ completed: true })
+      .rightJoin('user','queue.user_name', 'user.user_name')
+      .leftJoin('user AS mentor', 'queue.mentor_user_name', 'mentor.user_name')
+      .orderBy('id', 'dsc');
+  },
+
 
 };
 
