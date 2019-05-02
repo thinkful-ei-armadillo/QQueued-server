40b195788cdca6834254246d42037c544c1fd07e
Jonathan Kleinpeter <joklein341@gmail.com>
2019-05-02 13:43:22 -0500

    almost done with feature

--- src/routes/data/dataService.js
+++ src/routes/data/dataService.js
@@ -15,6 +15,37 @@ const dataService = {
       .orderBy('id', 'dsc');
   },
 
+  getStudent(db, user_name) {
+    return db.from('queue')
+      .select(
+        'queue.id',
+        'queue.description',
+        'queue.user_name',
+        'user.full_name as studentName',
+        'mentor.full_name as mentorName'
+      )
+      .where({ completed: true })
+      .andWhere('queue.user_name', user_name)
+      .rightJoin('user', 'queue.user_name', 'user.user_name')
+      .leftJoin('user AS mentor', 'queue.mentor_user_name', 'mentor.user_name')
+      .orderBy('id', 'dsc');
+  },
+
+  getNotes(db) {
+    return db.from('studentData')
+      .select(
+        'studentData.user_name',
+        'note'
+      );
+  },
+
+  postNote(db, note, queue_id) {
+    return db
+      .into('studentData')
+      .where({ queue_id })
+      .update({ note }, ['user_name', 'note'])
+      .then(note => note);
+  }
 
 };
 
