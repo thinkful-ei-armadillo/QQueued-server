e14bb8bf84f16c95b84ade5439b55cb979a27372
matth3wn <matth3wn@gmail.com>
2019-05-02 02:26:08 -0700

    hacky chat

--- src/server.js
+++ src/server.js
@@ -16,25 +16,47 @@ app.set("db", db);
 app.set("socketio", io);
 io.set("origins", "*:*");
 
+let connectedClients = {};
+
 io.on("connection", async socket => {
   console.log("Client Successfully Connected");
-  socket.userName = 'Anon'
+  socket.userName = "Anon";
 
-  socket.on('change-username', data => {
-    socket.userName = data.userName
-    io.emit('entered', socket.userName)
-  })
- 
-  socket.on('message', data => {
-    io.emit('message', data)
-  })
-
-  socket.on('notifiy', data => {
-    io.emit('notifiy', data)
-  })
-  socket.on("disconnect", () => console.log("Client disconnected"));
+  socket.on("join-room", data => {
+    socket.userName = data.userName;
+    connectedClients[data.userName] = socket.id;
+    console.log(connectedClients);
+    console.log("joining room", connectedClients[data.userName]);
+    socket.join(connectedClients[data.userName]);
+    io.to(connectedClients[data.userName]).emit("entered", {
+      userName: socket.userName,
+      room: connectedClients[data.userName]
+    });
+  });
+  socket.on("message", data => {
+    console.log("sending from", data);
+    if (
+      data.to &&
+      (data.user === data.to.studentName || data.user === data.to.mentorName)
+    ) {
+      let id = connectedClients[data.to.studentName];
+      let id2 = connectedClients[data.to.mentorName];
+      console.log(id);
+      io.to(id).emit("message", data);
+      io.to(id2).emit("message", data);
+    }
+  });
+  socket.on("delete-ticket", data => {
+    io.emit("delete-ticket", data);
+  });
+  socket.on("notifiy", data => {
+    io.emit("notifiy", data);
+  });
+  socket.on("disconnect", () => {
+    delete connectedClients[socket.userName];
+    console.log("Client disconnected");
+  });
 });
-
 
 const getApiAndEmit = async socket => {
   try {
@@ -49,4 +71,3 @@ const getApiAndEmit = async socket => {
 http.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
-
