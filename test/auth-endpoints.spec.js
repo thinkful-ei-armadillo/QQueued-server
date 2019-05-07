213377234d567c94a2d5ebe2fe0cbe096ad5170e
Jonathan Kleinpeter <joklein341@gmail.com>
2019-05-07 12:17:43 -0500

    fix test-helpers bug

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -1,3 +1,5 @@
+/* global supertest */
+
 const knex = require('knex');
 const jwt = require('jsonwebtoken');
 const app = require('../src/app');
@@ -12,16 +14,41 @@ describe('Auth endpoints', () => {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: 'postgres://jon@localhost/capstone-III-test'
     });
     app.set('db', db);
   });
 
   after('disconnect from db', () => db.destroy());
   before('cleanup', () => helper.cleanTables(db));
-  afterEach('cleanup', () => helper.cleanTables(db));
-
+  before('create tables', () => helper.createTables(db));
+  
   describe('POST /api/auth', () => {
-    
-  })
+    afterEach('cleanup', () => helper.cleanTables(db));
+    beforeEach('insert user', () => {
+      helper.seedUser(
+        db,
+        testUsers
+      );
+    });
+
+    const requiredFields = ['password', 'name'];
+
+    requiredFields.forEach(f => {
+      const loginAttempt = {
+        name: testUser.user_name,
+        password: testUser.password
+      };
+
+      it(`responds with 400 required error when ${f} is missing`, () => {
+        delete loginAttempt[f];
+        
+        return supertest(app)
+          .post('/api/auth')
+          .send(loginAttempt)
+          .expect(400);
+      });
+    });
+
+  });
 });
