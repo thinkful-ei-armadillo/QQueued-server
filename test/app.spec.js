380a4efa3e43b6cbf215b964980cce3fca9669f6
Jonathan Kleinpeter <joklein341@gmail.com>
2019-05-07 09:16:56 -0500

    begin test-helpers.js

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,4 +1,4 @@
-'use strict';
+/* eslint-disable no-undef */
 
 const app = require('../src/app');
 
--- test/setup.js
+++ test/setup.js
@@ -1,7 +1,11 @@
-'use strict';
+process.env.TZ = 'UTC';
+process.env.NODE_ENV = 'test';
+process.env.JWT_SECRET = 'testing-git-rekt-capstone-secret';
+process.env.TEST_DB_URL = 'postgresql://jon@localhost/capstone-III-test';
 
 const { expect } = require('chai');
 const supertest = require('supertest');
+
 
 global.expect = expect;
 global.supertest = supertest;
