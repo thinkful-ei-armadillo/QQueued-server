41ab41d6135b83f2ea045944748c5b94dea9851c
Jonathan Kleinpeter <joklein341@gmail.com>
2019-05-08 17:21:31 -0500

    finish with rework

--- src/routes/data/dataService.js
+++ src/routes/data/dataService.js
@@ -21,23 +21,19 @@ const dataService = {
   getData(db) {
     return db.from('queue')
       .select(
-        'queue.description',
         'queue.id',
+        'queue.description',
         'queue.user_name',
-        'queue.completed',
-        'queue.dequeue',
-        'queue.next',
         'queue.room',
-        'queue.mentor_notes as mentorNotes',
-        'queue.student_notes as studentNotes',
-        'queue.slack_user_id',
+        'queue.mentor_notes',
+        'queue.student_notes',
         'user.full_name as studentName',
         'mentor.full_name as mentorName'
       )
-      .where({ completed: false })
+      .where({ completed: true })
       .rightJoin('user', 'queue.user_name', 'user.user_name')
       .leftJoin('user AS mentor', 'queue.mentor_user_name', 'mentor.user_name')
-      .orderBy('id', 'asc');
+      .orderBy('id', 'dsc');
   },
 
   getNotes(db) {
