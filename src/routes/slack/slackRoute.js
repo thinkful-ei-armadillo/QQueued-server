f5c95f0d14763c7520b4506745170c09b977dce8
Robin Khiv <robinkhiv@gmail.com>
2019-06-02 15:35:38 -0700

    added slack config for bot

--- src/routes/slack/slackRoute.js
+++ src/routes/slack/slackRoute.js
@@ -77,7 +77,7 @@ slackRouter.route('/message').post(parser, async (req, res, next) => {
 
   let con = {
     headers: {
-      Authorization: `Bearer ${config.SLACK_TOKEN}`
+      Authorization: `Bearer ${config.BOT_TOKEN}`
     }
   };
   const data = await axios
