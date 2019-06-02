7c447f8d023ae5f7c85f6cc2864588f8f6f1822a
Robin Khiv <robinkhiv@gmail.com>
2019-06-02 14:39:14 -0700

    added username to match slack in migration

--- migrations/004.do.create_seeds.sql
+++ migrations/004.do.create_seeds.sql
@@ -33,7 +33,7 @@ VALUES
     
   ),
   (
-    'student4',
+    'robinkhiv',
     'student',
     'Robin',
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
@@ -53,15 +53,15 @@ VALUES
     '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
   
-  INSERT INTO "queue" ("description", "user_name", "mentor_user_name", "dequeue", "completed", "next")
-  VALUES
-    ('help me i dont know what im doing', 'student2', 'professX', TRUE, FALSE, null),
-    ('help me i dont know what im doing', 'student3', 'QueenD', TRUE, FALSE, null),
-    ('help me i dont know what im doing', 'student4', NULL, FALSE, FALSE, 4),
-    ('help me i dont know what im doing', 'matth3wn', NULL, FALSE, FALSE, 5),
-    ('help me i dont know what im doing', 'student4', NULL, FALSE, FALSE, null);
+  -- INSERT INTO "queue" ("description", "user_name", "mentor_user_name", "dequeue", "completed", "next")
+  -- VALUES
+  --   ('help me i dont know what im doing', 'student2', 'professX', TRUE, FALSE, null),
+  --   ('help me i dont know what im doing', 'student3', 'QueenD', TRUE, FALSE, null),
+  --   ('help me i dont know what im doing', 'student4', NULL, FALSE, FALSE, 4),
+  --   ('help me i dont know what im doing', 'matth3wn', NULL, FALSE, FALSE, 5),
+  --   ('help me i dont know what im doing', 'student4', NULL, FALSE, FALSE, null);
 
   INSERT INTO "pointers"("id", "head", "tail")
   VALUES
-    (1, 3, 5);
+    (1, null, null);
 COMMIT;
